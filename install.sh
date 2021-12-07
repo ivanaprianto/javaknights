20c6243247659760cc24e230320a39f47b5ce946
ivanaprianto <ivan@technonatura.sch.id>
2021-12-07 15:14:04 +0700

    ee

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo rm -r /var/www/html/javaknights
+sudo cp -r ../javaknights/ /var/www/html/
+sudo rm /var/www/html/javaknights/readme.md
+sudo rm /var/www/html/javaknights/install.sh
+sudo rm -r ../javaknights/
